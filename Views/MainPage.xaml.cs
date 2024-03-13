8fe78fdcdcb4a3438bdfdb882cfc49569c4fb36c
Ethan Wright <me@wrighte.dev>
2024-03-13 16:58:12 -0700

    File explorer erros

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -3,16 +3,54 @@
 public partial class MainPage : ContentPage
 {
 	int _count;
+	string? _file;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private async void OnCounterClicked(object sender, EventArgs e)
+	private async void OnUploadClicked(object sender, EventArgs e)
 	{
-		_count++;
-		CounterBtn.Text = $"Current count: {_count}";
+		var localAppDataDir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+		
+		var pickOptions = new PickOptions();
+		string? file =  await CopyPickedToLocal(pickOptions, localAppDataDir);
+		
+		if (file != null)
+		{
+			_file = file;
+			UploadButton.Text = "Chosen file: " + file;
+		}
+	}
+	
+	private async Task<string>? CopyPickedToLocal(PickOptions options, string localAppDataDir)
+	{
+		try
+		{
+			var result = await FilePicker.Default.PickAsync();
+			
+			if (result != null)
+			{
+				string localAudioFile = Path.Combine(localAppDataDir, result.FileName);
+				
+				using (var sourceStream = await result.OpenReadAsync())
+				using (var destinationStream = File.Create(localAudioFile))
+				{
+					await sourceStream.CopyToAsync(destinationStream);
+				}
+				
+				return localAppDataDir;
+			}
+
+			return null;
+		}
+		catch (Exception ex)
+		{
+			// The user canceled or something went wrong
+		}
+
+		return null;
 	}
 }
 
