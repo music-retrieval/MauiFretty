e3dd58acface67ecee5c58f37b97e920468a0b2f
alyssa-blair <alyssakmblair@gmail.com>
2024-03-31 23:25:06 -0700

    add string labels and middle circles

--- Views/FretBoard.xaml.cs
+++ Views/FretBoard.xaml.cs
@@ -4,32 +4,45 @@ using Microsoft.Maui.Controls;
 using Microsoft.Maui.Graphics;
 using System;
 using Microsoft.Maui;
-
+using System.Collections.Generic;
 public partial class FretBoard
 {
     private const int NumRows = 7;
     private const int NumCols = 15;
+    private readonly Dictionary<string, object[]> _strings = new Dictionary<string, object[]>
+    {
+        {"E", [0, 3, "#FF97BFFF"]},
+        {"B", [1, 7, "#FF97FFA3"]}, 
+        {"G", [2, 5, "#FFFF97F3"]}, 
+        {"D", [3, 2, "#FFFFD797"]}, 
+        {"A", [4, 6, "#FF75FEF6"]},
+    };
+
     public FretBoard()
     {
         InitializeComponent();
         GenerateGrid();
         GenerateFretBoard();
-        GenerateNote("E", 3, 3);
-
     }
 
     private void GenerateGrid()
     {
-        // TODO: make height and width dynamic
+        const double rowLength = 50;
+        const double colLength = 59;
+        
         for (var i = 0; i < NumRows; i++)
         {
-            Grid.RowDefinitions.Add(new RowDefinition {Height = new GridLength(50)});
+            Grid.RowDefinitions.Add(new RowDefinition {Height = new GridLength(rowLength)});
         }
         
-        for (var i = 0; i < NumCols; i++)
+        
+        for (var i = 0; i < NumCols+3; i++)
         {
-            Grid.ColumnDefinitions.Add(new ColumnDefinition {Width = new GridLength(59)});
+            // create full-width columns for middle and half-width for beginning and end of fretboard
+            Grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(
+                colLength / (i is > NumCols or < 2 ? 2 : 1))});
         }
+
     }
 
     private void GenerateFretBoard()
@@ -37,58 +50,64 @@ public partial class FretBoard
         // create the full-width vertical bars
         for (var i = 0; i < NumRows - 1; i++)
         {
-            for (var j = 1; j < NumCols + 1; j++)
+            for (var j = 2; j < NumCols + 2; j++)
             {
-                Grid.Add(GenerateBoxView(null, LayoutOptions.Start, j == 1 ? 2 : 0.5, null,
-                    color: j == 1 ? "#38753F" : "#000000"), j, i);
+                Grid.Add(GenerateBoxView(i, j, LayoutOptions.Start, null, j == 2 ? 2 : 0.5, null, color: j == 2 ? "#38753F" : "#000000"), j, i);
             }
         }
         
         // create the horizontal bars
         for (var i = 0; i < NumRows - 1; i++)
         {
-            for (var j = 1; j < NumCols; j++)
+            for (var j = 1; j <= NumCols + 1; j++)
             {
-                if (j == 1)
-                {
-                    // add the half length pieces
-                    Grid.Add(GenerateBoxView(null, LayoutOptions.End, 
-                        Grid.ColumnDefinitions[0].Width.Value/2, 0.5 * (i+1), color: "#000000"), 
-                        0, i);
-                    Grid.Add(GenerateBoxView(null, LayoutOptions.Start, 
-                        Grid.ColumnDefinitions[0].Width.Value/2, 0.5 * (i+1), color: "#000000"), 
-                        NumCols, i);
-                } 
-                Grid.Add(GenerateBoxView(null, null, null, 0.5 * (i+1), color: "#000000"), 
+                Grid.Add(GenerateBoxView(i, j, null, null, null, 0.5 * (i + 1), color: "#000000"), 
                     j, i);
             }
+        }
+            
+        
+        foreach (var (note, info) in _strings)
+        {
+            // add the string label and note on beginning of fretboard
+            var (index, number, color) = ((int)info[0], (int)info[1], (string)info[2]);
+            GenerateNote(note, "#00000000", 0, index, bolded: false);
+            GenerateNote(note, color, 1, index);
+            
+            if (index != 0) continue;
+            
+            // add the other E note
+            GenerateNote(note, "#00000000", 0, 5, bolded: false);
+            GenerateNote(note, color, 1, 5);
         }
 
     }
 
-    private BoxView GenerateBoxView(LayoutOptions? vertical, LayoutOptions? horizontal, double? width, double? height, 
-        string color="#000000")
+    
+    private BoxView GenerateBoxView(int i, int j, LayoutOptions? horizontal, LayoutOptions? vertical, 
+        double? width, double? height, string color="#000000")
         
     {
         var boxView = new BoxView
         {
             Color = Color.FromArgb(color),
-            HeightRequest = height ?? Grid.RowDefinitions[0].Height.Value,
-            WidthRequest = width ?? Grid.ColumnDefinitions[0].Width.Value,
+            HeightRequest = height ?? Grid.RowDefinitions[i].Height.Value,
+            WidthRequest = width ?? Grid.ColumnDefinitions[j].Width.Value,
             VerticalOptions = vertical ?? LayoutOptions.Center,
             HorizontalOptions = horizontal ?? LayoutOptions.Center,
         };
         
         return boxView;
     }
-    
-    private void GenerateNote(string note, int row, int col)
+
+    private void GenerateNote(string note, string color, int col, int row, bool bolded = true)
     {
+
         var circle = new Ellipse
         {
-            WidthRequest = 18,
-            HeightRequest = 18,
-            Fill = Colors.Red,
+            WidthRequest = 20,
+            HeightRequest = 20,
+            Fill = Color.FromArgb(color),
             HorizontalOptions = LayoutOptions.Center,
             VerticalOptions = LayoutOptions.Center,
         };
@@ -97,8 +116,10 @@ public partial class FretBoard
         {
             Text = note,
             FontSize = 16,
+            FontAttributes = bolded ? FontAttributes.Bold : FontAttributes.None,
+            TextColor = Colors.Black,
             HorizontalOptions = LayoutOptions.Center,
-            VerticalOptions = LayoutOptions.Center
+            VerticalOptions = LayoutOptions.Center,
         };
         
         Grid.Add(circle, col, row);
@@ -114,6 +135,5 @@ public partial class FretBoard
             button.FontAttributes == FontAttributes.Bold ? FontAttributes.None : FontAttributes.Bold;
 
     }
-    
     
 }
